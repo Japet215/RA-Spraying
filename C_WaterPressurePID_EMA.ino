a0d3e21cf107260625093b80fd3cceee6897ae44
Japet215 <wilterjapetm@gmail.com>
2026-03-18 09:36:17 +0800

    Initial commit

---
+++ C_WaterPressurePID_EMA.ino
@@ -0,0 +1,203 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//------------------------------------------------------------------------------------
+//---16x2 LCD (I2C)---
+//------------------------------------------------------------------------------------
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+//------------------------------------------------------------------------------------
+//---DC Motor Driver L293D---
+//------------------------------------------------------------------------------------
+//  Motor A (Left)
+#define forwardPin_A   8
+#define backwardPin_A  9
+#define enablePin_A   10
+
+//  Motor B (Right)
+#define forwardPin_B  12
+#define backwardPin_B 13
+#define enablePin_B   11
+
+//------------------------------------------------------------------------------------
+//---Potentiometers---
+//------------------------------------------------------------------------------------
+#define potPin A0
+int potVal = 0;
+
+#define potKp A1
+#define potKi A2
+#define potKd A3
+
+//------------------------------------------------------------------------------------
+//---Water Pressure Sensor---
+//------------------------------------------------------------------------------------
+#define pressurePin A6
+
+//------------------------------------------------------------------------------------
+//---PID Global Variables---
+//------------------------------------------------------------------------------------
+long previousTime = 0;
+float errorSum = 0;
+float previousError = 0;
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+//------------------------------------------------------------------------------------
+//--- Kalman Filter ---
+//------------------------------------------------------------------------------------
+
+// --- Kalman Filter Parameters ---
+float Q = 0.05;// Process noise covariance (smoothness)
+float R = 15.0;// Measurement noise covariance (noise level)
+float P = 1.0;// Estimation error covariance
+float X = 0.0;// Filtered value
+
+float kalmanFilter(float measurement) {
+  // Prediction update
+  P = P + Q;
+
+  // Measurement update
+  float K = P / (P + R);
+  X = X + K * (measurement - X);
+  P = (1 - K) * P;
+
+  return X;
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~SETUP~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+
+  // LCD Init
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("System Start");
+
+  // Motor Driver Pins
+  pinMode(forwardPin_A, OUTPUT);
+  pinMode(backwardPin_A, OUTPUT);
+  pinMode(enablePin_A, OUTPUT);
+
+  pinMode(forwardPin_B, OUTPUT);
+  pinMode(backwardPin_B, OUTPUT);
+  pinMode(enablePin_B, OUTPUT);
+
+  pinMode(pressurePin, INPUT);
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~LOOP~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void loop() {
+
+  long currentTime = micros();
+  float deltaTime = ((float)(currentTime - previousTime)) / 1e6;
+
+  //---Measure Pressure---
+  int rawValue = analogRead(pressurePin);
+  float voltage = rawValue * (5.0 / 1024.0);
+
+  float offset = 0.5;
+  float voltage_comp = voltage - offset;
+  float pressure_kPa = (voltage_comp) * 250;
+
+  /*
+  float pressureMeasured_raw = pressure_kPa; 
+  if(pressureMeasured_raw <= 0.0){
+    pressureMeasured_raw = 0.0;
+  }
+
+  // Initialize EMA filter variables
+  float alpha = 0.60;          // smoothing factor (0.0–1.0)
+
+  // ---- Apply EMA Filter ----
+  float pressureEMA = alpha * pressureMeasured_raw + (1 - alpha) * pressureEMA;
+
+  // Use filtered value as the real measurement
+  float pressureMeasured = pressureEMA;
+  */
+
+  float pressureRaw = pressure_kPa;
+  if(pressureRaw <= 0.0){
+    pressureRaw = 0.0;
+  }
+  float pressureMeasured = kalmanFilter(pressureRaw);
+
+  //---Setpoint---
+  potVal = analogRead(potPin);
+  float pressureDesired = map(potVal, 0, 1024, 0, 61);
+
+  //---Errors---
+  float error = pressureDesired - pressureMeasured;
+
+  if (error < 1) errorSum = 0;
+  else errorSum += error * deltaTime;
+
+  float dErr = (error - previousError) / deltaTime;
+
+  //---Read PID Gains---
+  float Kp = mapfloat(analogRead(potKp), 0, 1024, 0, 50.0);
+  float Ki = mapfloat(analogRead(potKi), 0, 1024, 0, 30.0);
+  float Kd = mapfloat(analogRead(potKd), 0, 1024, 0, 5.0);
+
+  float feedForward = 0.0;
+
+  float PIDinput = Kp * error + Ki * errorSum + Kd * dErr;
+  if (PIDinput > 255) PIDinput = 255;
+
+  // Motor Direction
+  digitalWrite(forwardPin_A, HIGH);
+  digitalWrite(backwardPin_A, LOW);
+
+  digitalWrite(forwardPin_B, HIGH);
+  digitalWrite(backwardPin_B, LOW);
+
+  // Actuate motors
+  float controlInputU_A = PIDinput + feedForward;
+  float controlInputU_B = -PIDinput + feedForward;
+
+  analogWrite(enablePin_A, controlInputU_A);
+  analogWrite(enablePin_B, controlInputU_B);
+
+  //---Display on LCD---
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("M:");
+  lcd.print(pressureMeasured, 0);
+  lcd.print(" T:");
+  lcd.print(pressureDesired, 0);
+  lcd.print(" E:");
+  lcd.print(error, 0);
+
+  lcd.setCursor(0, 1);
+  lcd.print("U:");
+  lcd.print((int)controlInputU_A);
+  lcd.print(" P:");
+  lcd.print(Kp, 0);
+  lcd.print(" I:");
+  lcd.print(Ki, 0);
+  lcd.print(" D:");
+  lcd.print(Kd, 0);
+
+  // Serial Debug
+  Serial.print("pressureMeasured:");
+  Serial.print(pressureMeasured);
+  Serial.print(", pressureRaw:");
+  Serial.print(pressureRaw);
+  Serial.print(", pressureDesired:");
+  Serial.println(pressureDesired);
+
+  previousError = error;
+  previousTime = currentTime;
+  //delay(10);
+}
